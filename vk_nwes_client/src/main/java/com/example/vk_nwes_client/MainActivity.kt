87988bd96eee21eb7ca6b4e13aa0e986afb2586f
Margarita Starkova <mstarkova@rutube.ru>
2023-12-22 15:33:10 +0300

    2.11 VkNewsClient

--- vk_nwes_client/src/main/java/com/example/vk_nwes_client/MainActivity.kt
+++ vk_nwes_client/src/main/java/com/example/vk_nwes_client/MainActivity.kt
@@ -3,6 +3,13 @@ package com.example.vk_nwes_client
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.vk_nwes_client.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +17,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                PostCard()
+                Box(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background)
+                    .padding(8.dp)
+                )
+                { PostCard() }
             }
         }
     }
